c5e81b92a8f0eced61b73d7dd42c9eab1be912ea
Cesar Caceres <cesarc770@gmail.com>
2017-11-13 17:18:53 -0700

    initial commit all files from completed project

---
+++ assets/javascript/nav.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$("body").css("height", screen.height + "px");
+	$("#second-nav").css("height", screen.height + "px");
+
+	$("#start-btn").on("click", function(){
+		window.location.href = "pet-matching.html";
+	})
+	
+
+})
